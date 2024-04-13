d96e26a7ea7928cc138888d6a98bd7933865ce17
RishiAitha <rishi.aitha@gmail.com>
2024-04-12 21:47:57 -0400

    actual initial commit

---
+++ Apollo/Assets/CosmicMan/DemoScripts/BulletPool.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletPool  : MonoBehaviour
+{
+	public static BulletPool bulletPoolInstance;
+
+	public GameObject poolBullet;
+	public int totalBulletsInPool;
+
+	private bool needMoreBullets = true;
+	private List<GameObject> bulletList;
+
+	private void Awake()
+	{
+		bulletPoolInstance = this;
+	}
+
+	private void Start()
+	{
+		bulletList = new List<GameObject>();
+		totalBulletsInPool = totalBulletsInPool <= 0 ? 10 : totalBulletsInPool;
+	}
+
+	public GameObject GetBullet(int index)
+	{
+		//if (index > bulletList.Count - 1) return null;
+
+		if (bulletList.Count > 0 && !needMoreBullets)
+		{
+			if (!bulletList[index].activeInHierarchy)
+			{
+				return bulletList[index];
+			}
+		}
+		else
+		{
+			GameObject bullet = Instantiate(poolBullet);
+			bullet.SetActive(false);
+			bulletList.Add(bullet);
+
+			if (bulletList.Count == totalBulletsInPool)
+				needMoreBullets = false;
+
+			return bullet;
+		}
+
+		/*if (notEnoughtBulletsInPool)
+		{
+			GameObject bullet = Instantiate(poolBullet);
+			bullet.SetActive(false);
+			bulletList.Add(bullet);
+			return bullet;
+		}*/
+
+		return null;
+	}
+}
---
+++ Apollo/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,132 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D myRB;
+
+    public BoxCollider2D groundCheckBox;
+    public LayerMask groundLayer;
+
+    public float coyoteTime;
+    private float coyoteTimeCounter;
+
+    public Animator playerAnimator;
+
+    public float playerSpeed;
+    public float jumpSpeed;
+
+    private bool facingRight;
+
+    public float wallSlideSpeed;
+
+    public Vector3 wallJumpSpeed;
+    public float wallJumpDirection;
+
+    public float wallJumpCoyoteTime;
+    private float wallJumpCoyoteTimeCounter;
+
+    private bool wallJumping;
+
+    public BoxCollider2D wallCheckBox;
+
+    void Start()
+    {
+        myRB = GetComponent<Rigidbody2D>();
+        facingRight = true;
+    }
+
+    void Update()
+    {
+        if (IsGrounded())
+        {
+            coyoteTimeCounter = coyoteTime;
+        }
+        else
+        {
+            coyoteTimeCounter -= Time.deltaTime;
+        }
+
+        if (OnWall() && coyoteTimeCounter <= 0f && Input.GetAxisRaw("Horizontal") != 0f)
+        {
+            // we are wall sliding
+            myRB.velocity = new Vector3(myRB.velocity.x, -wallSlideSpeed, 0f);
+            wallJumpDirection = -transform.localScale.x;
+            wallJumpCoyoteTimeCounter = wallJumpCoyoteTime;
+            CancelInvoke("StopWallJumping");
+        }
+        else
+        {
+            wallJumpCoyoteTimeCounter -= Time.deltaTime;
+        }
+
+        if (!wallJumping)
+        {
+            if (Input.GetAxisRaw("Horizontal") > 0f)
+            {
+                myRB.velocity = new Vector3(playerSpeed, myRB.velocity.y, 0f);
+                if (!facingRight)
+                {
+                    Turn();
+                }
+            }
+            else if (Input.GetAxisRaw("Horizontal") < 0f)
+            {
+                myRB.velocity = new Vector3(-playerSpeed, myRB.velocity.y, 0f);
+                if (facingRight)
+                {
+                    Turn();
+                }
+            }
+            else
+            {
+                myRB.velocity = new Vector3(0f, myRB.velocity.y, 0f);
+            }
+        }
+
+        if (Input.GetButtonDown("Jump") && coyoteTimeCounter > 0f)
+        {
+            myRB.velocity = new Vector3(myRB.velocity.x, jumpSpeed, 0f);
+            coyoteTimeCounter = 0f;
+        }
+
+        if (Input.GetButtonDown("Jump") && wallJumpCoyoteTimeCounter > 0f)
+        {
+            wallJumping = true;
+            myRB.velocity = new Vector3(wallJumpDirection * wallJumpSpeed.x, wallJumpSpeed.y, 0f);
+            wallJumpCoyoteTimeCounter = 0f;
+
+            if (transform.localScale.x != wallJumpDirection)
+            {
+                Turn();
+            }
+
+            Invoke("StopWallJumping", 0.2f);
+        }
+
+        playerAnimator.SetBool("OnGround", coyoteTimeCounter > 0f);
+        playerAnimator.SetFloat("PlayerSpeed", Mathf.Abs(myRB.velocity.x));
+    }
+
+    public bool IsGrounded()
+    {
+        return Physics2D.OverlapBox(groundCheckBox.transform.position, groundCheckBox.size, 0f, groundLayer);
+    }
+
+    public bool OnWall()
+    {
+        return Physics2D.OverlapBox(wallCheckBox.transform.position, wallCheckBox.size, 0f, groundLayer);
+    }
+
+    private void Turn()
+    {
+        facingRight = !facingRight;
+        transform.localScale = new Vector3(-transform.localScale.x, 1f, 1f);
+    }
+
+    private void StopWallJumping()
+    {
+        wallJumping = false;
+    }
+}
