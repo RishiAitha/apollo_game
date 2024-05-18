236c67a7dea443c7264665fac4bb4bda7a5fb81f
RishiAitha <110874460+RishiAitha@users.noreply.github.com>
2024-05-17 23:14:27 -0400

    moving main menu

--- APOLLO/Assets/Scripts/MainMenu.cs
+++ APOLLO/Assets/Scripts/MainMenu.cs
@@ -9,11 +9,109 @@ public class MainMenu : MonoBehaviour
     public GameObject startMenu;
     public GameObject levelMenu;
 
+    public GameObject levelButtons;
+    public GameObject levelTiles;
+
+    public GameObject leftButton;
+    public GameObject rightButton;
+
+    public int currentLevel;
+
+    private bool movingButtons;
+
+    public float buttonMoveTime;
+    private float leftCounter;
+    private float rightCounter;
+    private float lerpCounter;
+    private Vector3 buttonTargetPos;
+    private Vector3 tilesTargetPos;
+
+    public bool resetLevels;
+    public bool unlockAllLevels;
+
     void Start()
     {
         OpenStartMenu();
 
         UnlockFirstLevel();
+
+        currentLevel = 1;
+    }
+
+    void Update()
+    {
+        if (!movingButtons)
+        {
+            if (currentLevel == 1)
+            {
+                leftButton.SetActive(false);
+            }
+            else
+            {
+                leftButton.SetActive(true);
+            }
+
+            if (currentLevel == 5 || !PlayerPrefs.HasKey("Level" + (currentLevel + 1)) || PlayerPrefs.GetInt("Level" + (currentLevel + 1)) < 0)
+            {
+                rightButton.SetActive(false);
+            }
+            else
+            {
+                rightButton.SetActive(true);
+            }
+        }
+        else
+        {
+            leftButton.SetActive(false);
+            rightButton.SetActive(false);
+        }
+
+        if (leftCounter > 0f || rightCounter > 0f)
+        {
+            movingButtons = true;
+        }
+        else
+        {
+            movingButtons = false;
+        }
+
+        if (leftCounter > 0f)
+        {
+            levelButtons.transform.position = Vector3.Lerp(levelButtons.transform.position, buttonTargetPos, lerpCounter / buttonMoveTime);
+            levelTiles.transform.position = Vector3.Lerp(levelTiles.transform.position, tilesTargetPos, lerpCounter / buttonMoveTime);
+            leftCounter -= Time.deltaTime;
+            lerpCounter += Time.deltaTime;
+        }
+
+        if (rightCounter > 0f)
+        {
+            levelButtons.transform.position = Vector3.Lerp(levelButtons.transform.position, buttonTargetPos, lerpCounter / buttonMoveTime);
+            levelTiles.transform.position = Vector3.Lerp(levelTiles.transform.position, tilesTargetPos, lerpCounter / buttonMoveTime);
+            rightCounter -= Time.deltaTime;
+            lerpCounter += Time.deltaTime;
+        }
+
+        if (resetLevels)
+        {
+            resetLevels = false;
+
+            PlayerPrefs.SetInt("Level1", 0);
+
+            for (int i = 2; i < 6; i++)
+            {
+                PlayerPrefs.DeleteKey("Level" + i);
+            }
+        }
+
+        if (unlockAllLevels)
+        {
+            unlockAllLevels = false;
+
+            for (int i = 1; i < 6; i++)
+            {
+                PlayerPrefs.SetInt("Level" + i, 0);
+            }
+        }
     }
 
     public void OpenStartMenu()
@@ -26,6 +124,24 @@ public class MainMenu : MonoBehaviour
     {
         startMenu.SetActive(false);
         levelMenu.SetActive(true);
+    }
+
+    public void Left()
+    {
+        currentLevel--;
+        buttonTargetPos = new Vector3(levelButtons.transform.position.x + 1200f, levelButtons.transform.position.y, 0f);
+        tilesTargetPos = new Vector3(levelTiles.transform.position.x + 12f, levelTiles.transform.position.y, 0f);
+        lerpCounter = 0f;
+        leftCounter = buttonMoveTime;
+    }
+
+    public void Right()
+    {
+        currentLevel++;
+        buttonTargetPos = new Vector3(levelButtons.transform.position.x - 1200f, levelButtons.transform.position.y, 0f);
+        tilesTargetPos = new Vector3(levelTiles.transform.position.x - 12f, levelTiles.transform.position.y, 0f);
+        lerpCounter = 0f;
+        rightCounter = buttonMoveTime;
     }
 
     public void LoadLevel(LevelButton button)
